6f00c64942932e39066d9ea72f305ed3534a73e4
Natasha <tashachitongo@gmail.com>
2025-11-01 15:52:14 +0200

    Add code to github

--- adminhome.php
+++ adminhome.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
+    <title>Admin Home</title>
     <link rel="stylesheet" href=  "styles.css">
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
 </head>
